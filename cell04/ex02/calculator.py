38d0b91e4c61b153ea0562f206e97865cadebd4a
crotchan <aiwo789528@gmail.com>
2025-03-11 11:39:25 +0700

    ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+# รับค่าจากผู้ใช้
+num1 = float(input("Give me the first number: "))  # รับตัวเลขแรก
+num2 = float(input("Give me the second number: "))  # รับตัวเลขที่สอง
+
+print("Thank you!")
+
+# คำนวณและแสดงผลลัพธ์
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
